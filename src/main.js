5921935f6e483207b340895aaeed63bc372ea7e9
Aleksey Dovbnya <custos11@gmail.com>
2022-11-30 00:44:43 +0300

    base PWA

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueSvgInlinePlugin from "vue-svg-inline-plugin";
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(VueSvgInlinePlugin);
